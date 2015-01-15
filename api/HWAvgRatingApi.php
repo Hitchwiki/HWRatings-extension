0ffbc867148fb3932e37af589e5ee7ab41fe4cdf
Olexandr Melnyk <me@omelnyk.net>
2015-01-15 23:07:40 +0200

    add rating by specified  (or current) user to HWAvgRatingApi

--- api/HWAvgRatingApi.php
+++ api/HWAvgRatingApi.php
@@ -1,27 +1,42 @@
 <?php
 class HWAvgRatingApi extends ApiBase {
   public function execute() {
+    global $wgUser;
+
     // Get parameters
     $params = $this->extractRequestParams();
 
     $page_ids = $params['pageid'];
     $pageObj = $this->getTitleOrPageId($params);
 
+    if ($params['user_id'])
+      $user_id = $params['user_id'];
+    else
+      $user_id = $wgUser->getId();
+
     $dbr = wfGetDB( DB_SLAVE );
     $res = $dbr->select(
-      'hw_ratings_avg',
+      array('hw_ratings_avg', 'hw_ratings'),
       array(
         'hw_average_rating',
         'hw_count_rating',
-        'hw_page_id'
+        'hw_ratings_avg.hw_page_id',
+        'hw_ratings.hw_rating'
       ),
-      'hw_page_id IN ('.implode(',', $page_ids).')'
+      'hw_ratings_avg.hw_page_id IN ('.implode(',', $page_ids).')',
+      __METHOD__,
+      array(),
+      array( 'hw_ratings' => array( 'LEFT JOIN', array(
+        'hw_ratings.hw_page_id = hw_ratings_avg.hw_page_id',
+        'hw_ratings.hw_user_id = ' . $user_id
+      ) ) )
     );
     foreach( $res as $row ) {
       $vals = array(
         'pageid' => $row->hw_page_id,
         'rating_average' => $row->hw_average_rating,
-        'rating_count' => $row->hw_count_rating
+        'rating_count' => $row->hw_count_rating,
+        'rating_user' => $row->hw_rating ? $row->hw_rating : "0"
       );
       $this->getResult()->addValue( array( 'query', 'ratings' ), null, $vals );
     }
@@ -43,6 +58,10 @@ class HWAvgRatingApi extends ApiBase {
               ApiBase::PARAM_TYPE => 'integer',
               ApiBase::PARAM_REQUIRED => true,
               ApiBase::PARAM_ISMULTI => true
+          ),
+          'user_id' => array (
+              ApiBase::PARAM_TYPE => 'integer',
+              ApiBase::PARAM_REQUIRED => false
           )
       );
   }
